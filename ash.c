5fbeb88afc7a34be7d8b9709190616def4a301ff
Ashutoshrai14 <49158455+Ashutoshrai14@users.noreply.github.com>
2019-04-02 12:01:20 +0530

    Update ash.c

--- ash.c
+++ ash.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
-main()
+#include<conio.h>
+
+void main()
 {
- printf("hello world");
+char s[]="india";
+int i,j;
+clrscr();
+for(i=0;s[i];i++)
+{
+for(j=0;j<=i;j++)
+printf("%c",s[j]);
+printf("\n");
+}
+getch();
 }
